cec4b7dafbb3ba27494638ba577e7de7ac778958
hinton-lance <hinton.lance@gmail.com>
2024-12-01 19:40:19 -0600

    updated client

--- whiteboard/src/client.cpp
+++ whiteboard/src/client.cpp
@@ -1,70 +1,87 @@
 #include "client.h"
 #include <thread>
 
-client::client() {
-	//initiate winsock, error handling
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-		std::cerr << "Winsock initialization failed. Error: " << WSAGetLastError() << std::endl;
-		return;
-	}
-	//open socket and bind
-	sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-	if (sock == INVALID_SOCKET) {
-		std::cerr << "Socket initialization failed. Error: " << WSAGetLastError() << std::endl;
-		return;
-	}
+client::client() : running(true), readBuff(1024, 0) {
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "Winsock initialization failed. Error: " << WSAGetLastError() << std::endl;
+        running = false;
+        return;
+    }
 
-	server = false;
+    sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (sock == INVALID_SOCKET) {
+        std::cerr << "Socket initialization failed. Error: " << WSAGetLastError() << std::endl;
+        running = false;
+    }
 }
 
 client::~client() {
-	//cleanup
-	closesocket(sock);
-	WSACleanup();
+    running = false; // Stop threads
+    closesocket(sock);
+    WSACleanup();
 }
 
-client::send() {
-	while (true) {
-		//on 
+void client::send() {
+    while (running) {
+        // Handle GUI input and send corresponding packets
+    }
+}
+
+void client::receive() {
+    while (running) {
+        std::fill(readBuff.begin(), readBuff.end(), 0); // Clear the buffer
+        sockaddr_in from;
+        int fromSize = sizeof(from);
+        int bytesReceived = recvfrom(sock, readBuff.data(), readBuff.size(), 0, (sockaddr*)&from, &fromSize);
+        if (bytesReceived > 0) {
+            uint8_t type = readBuff[0];
+            handlePacket(type);
+        }
+    }
+}
+
+void client::handlePacket(uint8_t type) {
+    switch (type) {
+    case 0x03: // Disconnect
+        running = false; // Stop threads
+        break;
+    case 0x04: { // Update client list
+        clientIPs.clear();
+        const char* data = readBuff.data() + 1; // Skip packet type
+        while (*data) {
+            sockaddr_in addr = {};
+            if (inet_pton(AF_INET, data, &addr.sin_addr) > 0) {
+                clientIPs.push_back(addr);
+            }
+            data += strlen(data) + 1; // Move to the next string
+        }
+        break;
+    }
+    case 0x05: // Update whiteboard
+        // To be implemented
+        break;
+    }
+}
+
+void client::sendPacket(unsigned int type, const std::vector<char>& payload, const sockaddr_in& client) {
+	std::vector<char> packet;
+	packet.push_back(type);
+	packet.insert(packet.end(), payload.begin(), payload.end());
+
+	if (sendto(sock, packet.data(), packet.size(), 0, (sockaddr*)&client, sizeof(client)) == SOCKET_ERROR) {
+		std::cerr << "Failed to send packet to " << inet_ntoa(client.sin_addr) << ". Error: " << WSAGetLastError() << std::endl;
 	}
-}
-
-client::receive() {
-	while (true) {
-		sockaddr_in from;
-		int fromSize = sizeof(from);
-		int bytesReceived = recvfrom(sock, readBuff, sizeof(readBuff), 0, (sockaddr*)&from, &fromSize);
-		if (bytesReceived > 0) {
-			handlePacket();
-		}
-	}
-}
-
-client::signalHandler(int signal) {
-	buildPacket(2/*placeholder to build disconnect packet and send */);
-	exit(0);
-}
-
-client::buildPacket() {
-
-}
-
-client::handlePacket() {
-
-}
-
-client::updateWhiteboard() {
 
 }
 
 
 int main() {
 	client c;
-	signal(SIGINT, [](int signal) { c.signalHandler(signal); });
 	std::thread sendThread(&client::send, &c);
 	std::thread receiveThread(&client::receive, &c);
 
 	sendThread.join();
 	receiveThread.join();
 
+    delete c;
 }
--- whiteboard/src/client.h
+++ whiteboard/src/client.h
@@ -9,10 +9,17 @@
 
 #pragma comment(lib, "ws2_32.lib") // Automatically link the Winsock library
 
-struct packet {
-	uint_8 type; //0x01 for connect, 0x02 for disconnect, 0x03 for client update, 0x04 for whiteboard command, 0x05 for whiteboard mask, 0x06 for whiteboard color
+/*struct packet {
+	uint_8 type; //0x01 for connect, 0x02 for disconnect, 0x03 for session end, 0x04 for client update, 0x05 for whiteboard command, 0x06 for whiteboard mask, 0x07 for whiteboard color
 	char[] payload;
-};
+};*/
+
+//custom equality operator for sockaddr_in
+inline bool operator==(const sockaddr_in& a, const sockaddr_in& b) {
+	return a.sin_family == b.sin_family &&
+		a.sin_port == b.sin_port &&
+		a.sin_addr.s_addr == b.sin_addr.s_addr;
+}
 
 
 Class client{
@@ -21,8 +28,6 @@ Class client{
 		~client();
 		void send();
 		void receive();
-		void signalHandler(int signal);
-		void buildPacket(uint_8 type);
 		void handlePacket();
 		void updateWhiteboard();
 
@@ -30,10 +35,10 @@ Class client{
 		WSADATA wsaData;
 		SOCKET sock;
 
-		const bool server;
+		void sendPacket(unsigned int type, const std::vector<char>& payload, const sockaddr_in& client);
 
-		char* readBuff;
-		char* sendBuff;
+
+		std::vector<char> readBuff;
 
 		std::vector<sockaddr_in> clientIPs; //list of each clients IP for transmission to clients
 		//Whiteboard whiteboard; //instance of whiteboard sent to new members
