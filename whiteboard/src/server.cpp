945468908a1d0cde4fc75b3c6776289bb241fd89
hinton-lance <hinton.lance@gmail.com>
2024-12-01 18:46:48 -0600

    update server

--- whiteboard/src/server.cpp
+++ whiteboard/src/server.cpp
@@ -2,66 +2,149 @@
 
 
 server::server() {
-	server = true;
+	//initiate winsock, error handling
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+		std::cerr << "Winsock initialization failed. Error: " << WSAGetLastError() << std::endl;
+		exit(1);
+	}
+	sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (sock == INVALID_SOCKET) {
+		std::cerr << "Socket initialization failed. Error: " << WSAGetLastError() << std::endl;
+	}
+
+	sockaddr_in serverAddr = {};
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(8080);
+
+	if (bind(sock, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+		std::cerr << "Bind failed. Error: " << WSAGetLastError() << std::endl;
+		closesocket(sock);
+		WSACleanup();
+		exit(1);
+	}
+
+	std::cout << "Server started" << std::endl;
 }
 
 server::~server() {
+
+	closesocket(sock);
+	WSACleanup();
 }
 
-/*void server::createInviteCode() {
-	srand(clock(0));
-	inviteCode = "";
-	int rng;
-	bool case; //true = uppercase, false = lowercase
-	int letter; //standard alpabet range
-	for (int i = 0; i < 6; i++) {
-		rng = rand();
-		case = rng % 2;
-		letter = rng % 26;
-		if (case) {
-			inviteCode += static_cast<char>(letter + 65); //ascii offset for uppercase characters
-		}
-		else {
-			inviteCode += static_cast<char>(letter + 97); //ascii offset for lowercase characters
-		}
+void server::updateClients() {
+	//send updated client list to all clients
+	std::vector<char> clientList;
+	for (auto ip : clientIPs) {
+
+		char ipStr[INET_ADDRSTRLEN];
+		inet_ntop(AF_INET, &ip.sin_addr, ipStr, INET_ADDRSTRLEN);
+		clientList.insert(clientList.end(), ipStr, ipStr + INET_ADDRSTRLEN);
+		clientList.push_back('\0');
 	}
-}*/
 
-void server::send() override {
-
-}
-
-void server::receive() override {
-
+	for (auto ip : clientIPs) {
+		sendPacket(0x04, clientList, ip);
+	}
 }
 
 void server::addClient(sockaddr_in ip) {
-	if (std::find(clientIPs.begin(), clientIPs.end(), ip)) {
+	if (std::find(clientIPs.begin(), clientIPs.end(), ip) != clientIPs.end()) {
 		std::cerr << "Client is already connected" << std::endl;
 		return;
 	}
 
 	clientIPs.push_back(ip);
 
+	//send current whiteboard to client
+	std::vector<char> mask;
+	//buildWhiteboardMask(mask);
+	sendPacket(0x06, mask, ip);
+
 	updateClients();
+
 
 }
 
-void server::handleDisconnect(sockaddr ip) {
+
+//TODO
+
+//this will need to be fixed but just place holder for now
+//void server::buildWhiteboardMask(std::vector<char>& mask) {
+//	//build the whiteboard mask to send to clients
+//	for (int y = 0; y < WINDOW_HEIGHT; y++) {
+//		for (int x = 0; x < WINDOW_WIDTH; x++) {
+//			if (mask[y][x]) {
+//				//serialize?
+//			}
+//		}
+//	}
+//}
+
+void server::handleDisconnect(sockaddr_in ip) {
 	auto it = std::find(clientIPs.begin(), clientIPs.end(), ip);
 	if (it != clientIPs.end()) {
 		clientIPs.erase(it);
-	} else {
+		updateClients();
+	}
+	else {
 		std::cerr << "IP not found in client list" << std::endl;
+	}
+
+
+}
+
+void server::sendPacket(unsigned int type, const std::vector<char>& payload, const sockaddr_in& client) {
+	std::vector<char> packet;
+	packet.push_back(type);
+	packet.insert(packet.end(), payload.begin(), payload.end());
+
+	if (sendto(sock, packet.data(), packet.size(), 0, (sockaddr*)&client, sizeof(client)) == SOCKET_ERROR) {
+		std::cerr << "Failed to send packet to " << inet_ntoa(client.sin_addr) << ". Error: " << WSAGetLastError() << std::endl;
 	}
 
 }
 
-void endSession() {
-	
+void server::endSession() {
+	//send disconnect packet to all
+	for (auto ip : clientIPs) {
+		sendPacket(0x03, {}, ip);
+	}
+
+	clientIPs.clear();
 }
 
-int main() {
-
-}
+//int main() {
+//	server myServer;
+//	char buffer[1024];
+//	sockaddr_in clientAddr;
+//	int clientAddrSize = sizeof(clientAddr);
+//
+//	while (true) {
+//		int bytesReceived = recvfrom(myServer.sock, buffer, sizeof(buffer), 0, (sockaddr*)&clientAddr, &clientAddrSize);
+//		if (bytesReceived == SOCKET_ERROR) {
+//			std::cerr << "Error receiving packet." << WSAGetLastError() << std::endl;
+//			continue;
+//		}
+//
+//
+//		uint8_t packetType = buffer[0]; //assuming first byte is packet type
+//		std::vector<char> payload(buffer + 1, buffer + bytesReceived);
+//
+//		switch (packetType) {
+//		case 0x01:
+//			myServer.addClient(clientAddr);
+//			break;
+//		case 0x02:
+//			myServer.handleDisconnect(clientAddr);
+//			break;
+//		case 0x05:
+//			//do whiteboard update
+//			break;
+//		default:
+//			std::cerr << "Unknown packet received: " << (int)packetType << std::endl;
+//			break;
+//		}
+//	}
+//}
 
--- whiteboard/src/server.h
+++ whiteboard/src/server.h
@@ -11,8 +11,8 @@
 
 #pragma comment(lib, "ws2_32.lib") // Automatically link the Winsock library
 
-/*Author: Lance Hinton
-* Intended functionality: 
+/*
+* Intended functionality:
 * server sits in infinite loop receiving packets
 * case 1: packet is a new client:
 * add client IP to vector, send current whiteboard state to new client
@@ -24,19 +24,30 @@
 * this ensures the whiteboard is up to date for new clients joining the session
 */
 
-Class server : public client{
-	public:
-		server();
-		~server();
-		void send() override;
-		void receive() override;
-		void addClient(sockaddr_in ip);
-		void handleDisconnect(sockaddr_in ip);
-		void endSession();
+//custom equality operator for sockaddr_in
+inline bool operator==(const sockaddr_in& a, const sockaddr_in& b) {
+    return a.sin_family == b.sin_family &&
+        a.sin_port == b.sin_port &&
+        a.sin_addr.s_addr == b.sin_addr.s_addr;
+}
+class server {
+public:
+    server();
+    ~server();
+    void addClient(sockaddr_in ip);
+    void handleDisconnect(sockaddr_in ip);
+    void endSession();
+    SOCKET sock;
 
-	private:
-		//std::string inviteCode;
-		
-		//void createInviteCode();
-		void updateClients(); //sends a copy of the clientIP vector
+
+private:
+    //std::string inviteCode;
+    WSADATA wsaData;
+
+    std::vector<sockaddr_in> clientIPs; //list of each clients IP
+
+    //void createInviteCode();
+    void updateClients(); //sends a copy of the clientIP vector
+    void sendPacket(unsigned int type, const std::vector<char>& payload, const sockaddr_in& client);
+    //void buildWhiteboardMask(std::vector<char>& mask);
 };
